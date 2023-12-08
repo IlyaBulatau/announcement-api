f58c25fda1f6eabbd8a451370b443bac9547ff82
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-08 18:15:29 +0200

    mod script app file

--- scripts/app-entrypoint.sh
+++ scripts/app-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export $(grep -v '^#' ./env/.env | xargs)
-poetry run uvicorn src.main:app --port=$APP_PORT --host=$APP_HOST --reload
+poetry run uvicorn src.main:app --port=$APP_PORT --host=$APP_HOST --reload --log-level info
