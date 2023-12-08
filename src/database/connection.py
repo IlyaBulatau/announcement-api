2d147f5162d63210fccf629d073cc5d311f85a62
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-08 16:38:26 +0200

    added setting for connection to DB and created connection to database

---
+++ src/database/connection.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from src.settings import DatabaseSetting
+
+
+async_engine = create_async_engine(
+    url=DatabaseSetting().get_url(),
+    future=True,
+    )
+
+async_session = async_sessionmaker(
+    bind=async_engine, 
+    expire_on_commit=False,
+    class_=AsyncSession,
+    )
+
+async def get_session(commit: bool = True) -> AsyncSession:
+    session = async_session()
+    try:
+        yield session
+        if commit:
+            await session.commit()
+    except:
+        await session.rollback()
+    finally:
+        await session.close()
---
+++ src/settings.py
@@ -0,0 +1,30 @@
+from environs import Env
+from pathlib import Path
+
+
+BASE_DIR = Path.parent
+
+ENV_FILE = ".env"
+ENV = Env()
+ENV.read_env(path=BASE_DIR / "env" / ENV_FILE)
+
+
+class DatabaseSetting:
+    POSTGRES_USER: str = ENV("POSTGRES_USER", "root")
+    POSTGRES_PASSWORD: str = ENV("POSTGRES_PASSWORD", "postgres")
+    POSTGRES_DB: str = ENV("POSTGRES_DB", "name")
+    POSTGRES_PORT: str | int = ENV("POSTGRES_PORT", "5432")
+    POSTGRES_HOST: str = ENV("POSTGRES_HOST", "postgres")
+
+
+    def get_url(self) -> str:
+        url = \
+        "postgresql+asyncpg://{user}:{password}@{host}:{port}/{db}"
+        
+        return url.format(
+            user=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            db=self.POSTGRES_DB,
+        )
