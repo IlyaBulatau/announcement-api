7b066508700ad943e769d2a13cd963281a36794b
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-08 18:21:58 +0200

    fix bug

--- src/database/models/user.py
+++ src/database/models/user.py
@@ -7,6 +7,6 @@ from src.database.models.base import BaseModel
 class User(SQLAlchemyBaseUserTableUUID, BaseModel):
     __tablename__ = "users"
 
-    username = orm.Mapped[str] = orm.mapped_column(
+    username: orm.Mapped[str] = orm.mapped_column(
         String(length=32), unique=True, nullable=False
         )
