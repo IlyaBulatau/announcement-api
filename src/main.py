446b1ef30bc0a2d265d720e95555717a686ce1f4
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-08 19:33:29 +0200

    add setup auth router in main.py file

--- src/main.py
+++ src/main.py
@@ -1,25 +1,30 @@
 from fastapi import FastAPI, APIRouter
 
 from src import endpoints
+from src.apps.auth import auth_setup_router
 
 
 API_ROOT_URL = "/api/v1"
 
 main_router = APIRouter(prefix=API_ROOT_URL)
 
-def setup_router(main_router: APIRouter = main_router) -> None:
+
+def setup_router(app: FastAPI, main_router: APIRouter = main_router) -> None:
     """
-    It gets all router from endpoints module and include them to app
+    It gets all router and include them to app
     """
     main_router.include_router(
-        endpoints.healthcheck_router, prefix="/healthcheck", tags=["heatlcheck"]
+        endpoints.healthcheck_router, prefix="/healthcheck", tags=["heatlchecks"]
         )
-    
+    auth_setup_router(main_router)
+
     app.include_router(main_router)
+
+
 
 app = FastAPI(
     docs_url=API_ROOT_URL+"/docs",
-    redoc_url=API_ROOT_URL+"/redoc")
+    redoc_url=API_ROOT_URL+"/redoc",
+    )
 
-setup_router()
-
+setup_router(app)
