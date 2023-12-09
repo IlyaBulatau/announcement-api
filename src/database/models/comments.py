43c83b218c06db3da5a97335d8ff5bf640e9836c
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-09 21:42:58 +0200

    added new model - Comment

---
+++ src/database/models/comments.py
@@ -0,0 +1,21 @@
+import sqlalchemy as db
+from sqlalchemy import orm
+
+import uuid
+
+from src.database.models.base import BaseModel
+
+
+CONTENT_MAX_LENGTH = 256
+
+class Comment(BaseModel):
+    __tablename__ = "comments"
+
+    id: orm.Mapped[db.UUID] = orm.mapped_column(
+        db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
+    )
+    content: orm.Mapped[db.String] = orm.mapped_column(
+        db.String(length=CONTENT_MAX_LENGTH), nullable=False
+    )
+    announcement_id: orm.Mapped[db.UUID] = orm.mapped_column(db.ForeignKey("announcements.id"))
+    user_id: orm.Mapped[db.UUID] = orm.mapped_column(db.ForeignKey("users.id"))
