23523512db3168d8379ddabac50e886704a4ffbd
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-09 17:20:08 +0200

    create new app and add schemas

---
+++ src/apps/announcements/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+from fastapi_users.models import ID
+from datetime import datetime
+
+from src.database.models.category import EnumCategory
+
+
+class AnnouncementCreate(BaseModel):
+    title: str
+    content: str
+    category: EnumCategory
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True 
+
+class AnnouncementRead(AnnouncementCreate):
+    id: ID
+    user_id: ID
+    category: str
+    created_on: datetime
+
+
+class AnnouncementShortcut(BaseModel):
+    id: ID
+    title: str
