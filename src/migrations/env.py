1df56a801b6c52f567bd7fa7f9d5a41e749ce7b0
IlyaBulatau <ilyabulatau@gmail.com>
2023-12-09 17:21:08 +0200

    add migrations

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -9,7 +9,7 @@ from alembic import context
 
 from src.database.models.base import BaseModel
 from src.settings import DatabaseSetting
-from src.database.models import User
+from src.database.models import User, Announcement, Category
 
 
 settings = DatabaseSetting()
